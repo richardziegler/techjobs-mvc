89cee07ae783c86ee56622f16d0d1d7d540dc731
Richard Ziegler <rzieglerdevelopment@gmail.com>
2018-05-26 13:22:11 -0500

    Initial Commit

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,13 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "search/results")
+    public String searchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute(searchTerm);
+        model.addAttribute(searchTerm);
+
+        return "search";
+    }
 
 }
